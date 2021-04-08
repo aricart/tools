97b1b2c8b67c447390c0296618254367579822da
Alberto Ricart <alberto@synadia.com>
2021-04-08 10:44:37 -0500

    wip

--- mon.ts
+++ mon.ts
@@ -1,15 +1,31 @@
-import { cli, Flags } from "https://deno.land/x/cobra@v0.0.4/mod.ts";
+import { cli, Flags } from "https://deno.land/x/cobra@v0.0.5/mod.ts";
 import {
   connect,
   ConnectionOptions,
   credsAuthenticator,
   jwtAuthenticator,
   NatsConnection,
-  StringCodec,
 } from "https://deno.land/x/nats/src/mod.ts";
-import { MsgHdrsImpl } from "https://deno.land/x/nats/nats-base-client/internal_mod.ts";
+import {
+  MsgHdrsImpl,
+  NatsConnectionImpl,
+} from "https://deno.land/x/nats/nats-base-client/internal_mod.ts";
+import { green, red, yellow } from "https://deno.land/std/fmt/colors.ts";
 
-const sc = StringCodec();
+function bytes(bytes: number): string {
+  const pre = ["K", "M", "G", "T", "P", "E"];
+  const post = "B";
+  if (bytes < 1024) {
+    return `${bytes.toFixed(2)}${post}`;
+  }
+  const exp = parseInt(Math.log(bytes) / Math.log(1024) + "");
+  const index = parseInt((exp - 1) + "");
+  return `${(bytes / Math.pow(1024, exp)).toFixed(2)}${pre[index]}${post}`;
+}
+
+function numbers(n: number) {
+  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 async function conn(flags: Flags): Promise<NatsConnection> {
   let s = flags.value<string>("server");
@@ -22,7 +38,7 @@ async function conn(flags: Flags): Promise<NatsConnection> {
   }
   const opts = {
     servers: [s],
-    debug: !!flags.value<boolean>("debug"),
+    debug: flags.value<boolean>("debug"),
   } as ConnectionOptions;
 
   if (flags.value<string>("creds")) {
@@ -50,9 +66,21 @@ type metric = {
   ave?: number;
 };
 
-const reqmon = cli({
-  use: "reqmon [--debug] [--server host:port]",
-  short: "track req/resp",
+const mon = cli({
+  use: "mon [--debug] [--server host:port] [--max-wait secs]",
+  short: "monitor messages flowing through NATS",
+  long: `The monitor will print a single line updating every second
+showing the following statistics:
+
+MSGS  - number of messages seen
+RPEND - number of pending requests
+ROK   - number of requests that received a response
+RTIMO - number of requests that didn't receive a response 
+        within --max-wait seconds
+ERR   - number of messages that sported an error code header
+MAX   - maximum payload size seen
+AVG   - average payload size
+`,
   run: async (cmd, args, flags): Promise<number> => {
     type rr = {
       s: string;
@@ -61,66 +89,80 @@ const reqmon = cli({
       err?: string;
     };
     const pending = new Map<string, rr>();
-    const rtt: number[] = [];
+    let ok = 0;
+    let rtt = 0;
     let errors = 0;
     let timedout = 0;
-    let lastServiced = 0;
     const maxWait = flags.value<number>("max-wait") * 1000;
-
     let line = 0;
+    let payload = 0;
+    let maxPayload = 0;
+    let count = 0;
 
     setInterval(async () => {
       const now = Date.now();
-      let waits: number[] = [];
+      let waitCounts = 0;
+      let waits = 0;
       pending.forEach((v, k) => {
         if (now - v.st > maxWait) {
           timedout++;
           pending.delete(v.r);
         } else {
-          waits.push(now - v.st);
+          waits += now - v.st;
+          waitCounts++;
         }
       });
-      const wait: metric = { name: "waiting", count: waits.length };
-      if (waits.length) {
-        const sum = waits.reduce((pv, v): number => {
-          return pv + v;
-        });
-        wait.ave = sum / waits.length;
-      }
-      const serviced: metric = { name: "serviced", count: rtt.length };
-      if (rtt.length) {
-        const rtts = rtt.reduce((pv, v): number => {
-          return pv + v;
-        });
-        serviced.ave = rtts / rtt.length;
-      }
-      const errs = { name: "errors", count: errors };
-      const to = { name: "timeouts", count: timedout };
+      const wait: metric = { name: "RPEND", count: waitCounts };
+      wait.ave = waits / waits;
 
-      if (line) {
-        Deno.stdout.writeSync(sc.encode("".padEnd(line, "\b")));
+      const serviced: metric = { name: "ROK", count: ok };
+      if (ok) {
+        serviced.ave = rtt / ok;
       }
-      const s =
-        `waiting: ${wait.count} serviced: ${serviced.count} errs: ${errs.count} timedout: ${to.count}`;
+      const total = { name: "MSGS", count: count };
+      const to = { name: "RTIMO", count: timedout };
+      const errs = { name: "ERR", count: errors };
+
+      cmd.stdout("\r");
+
+      const buf = [total, serviced, wait, to, errs];
+      let color;
+      const lines = buf.map((e) => {
+        color = (e.name !== "ROK" && e.name !== "MSGS") && e.count > 0
+          ? red
+          : green;
+        color = e.name === "RPEND" && e.count > 0 ? yellow : color;
+        return `${e.name}: ${color(numbers(e.count))}`;
+      });
+      lines.push(
+        `MAX: ${bytes(maxPayload)} AVG: ${
+          bytes(payload ? payload / count : 0)
+        }`,
+      );
+      const s = lines.join(" ");
       line = s.length;
-      Deno.stdout.writeSync(sc.encode(s));
+      cmd.stdout(s);
     }, 1000);
 
     const nc = await conn(flags);
-    console.log(`connected`);
+    const nci = nc as unknown as NatsConnectionImpl;
+    cmd.stdout(`${green(`connected`)} ${nci.protocol.server.toString()}`);
     const sub = nc.subscribe(">");
     const done = (async () => {
       for await (const m of sub) {
+        count++;
+        payload += m.data.length;
+        maxPayload = Math.max(m.data.length, maxPayload);
         let r = pending.get(m.subject);
         if (r) {
-          rtt.push(Date.now() - r.st);
+          rtt += Date.now() - r.st;
           if (m.headers) {
             if ((m.headers as MsgHdrsImpl).hasError) {
               errors++;
             }
           }
           pending.delete(m.subject);
-          continue;
+          ok++;
         } else if (m.reply) {
           r = {
             s: m.subject,
@@ -136,54 +178,54 @@ const reqmon = cli({
     return Promise.resolve(0);
   },
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "max-wait",
   default: 60,
   usage: "max wait before forgetting message",
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "server",
   default: "",
   type: "string",
   persistent: true,
   usage: "server hostport",
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "demo",
   default: "",
   type: "boolean",
   persistent: true,
   usage: "use demo.nats.io",
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "test",
   default: "",
   type: "boolean",
   persistent: true,
   usage: "use connect.ngs.synadia-test.com:4222",
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "ngs",
   default: "",
   type: "boolean",
   persistent: true,
   usage: "use connect.ngs.global:4222",
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "debug",
   short: "d",
   type: "boolean",
   persistent: true,
   usage: "enable protocol debugging",
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "creds",
   short: "c",
   type: "string",
   persistent: true,
   usage: "filepath to creds file",
 });
-reqmon.addFlag({
+mon.addFlag({
   name: "jwt",
   short: "j",
   type: "string",
@@ -191,4 +233,4 @@ reqmon.addFlag({
   usage: "filepath to jwt file",
 });
 
-Deno.exit(await reqmon.execute(Deno.args));
+Deno.exit(await mon.execute(Deno.args));
